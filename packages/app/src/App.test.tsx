111785ba3aa9e2a45af007a86ab79d8e608cb707
Brian Pfretzschner <brian.pfretzschner@gmail.com>
2025-10-04 13:37:18 +0200

    Make solo game mode test deterministic

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -1,26 +1,27 @@
 import App from "#App.tsx";
 import { useStateStore } from "#state/state.ts";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { findMoveRandom } from "@tic-tac-toe/shared/algorithms";
 import { describe, expect, it } from "vitest";
 
 describe("App", () => {
-  it("can play a solo game", async () => {
+  it("can play a solo game - human wins", async ({ annotate }) => {
+    const testMoves = [0, 3, 4, 5];
+
     render(<App />);
 
     // Start solo game
     fireEvent.click(screen.getByRole("button", { name: "Start" }));
 
     // Repeat player and computer moves until the game is finished
-    while (useStateStore.getState().gameSession?.status !== "finished") {
+    for (const testMove of testMoves) {
+      await annotate(`move ${testMove}...`);
+
       await waitFor(() => {
         expect(screen.getByText("It's your turn...")).toBeDefined();
       });
 
       // Perform human turn
-      const cellIdx = findMoveRandom(useStateStore.getState().boardCells);
-      expect(cellIdx).toBeDefined();
-      useStateStore.getState().gameSession?.requestPlayerMove(cellIdx!);
+      useStateStore.getState().gameSession?.requestPlayerMove(testMove);
 
       await waitFor(() => {
         expect(useStateStore.getState().gameSession?.nextTurn).toBe("computer");
@@ -32,6 +33,6 @@ describe("App", () => {
       });
     }
 
-    expect(useStateStore.getState().gameSession?.status).toBe("finished");
+    expect(screen.getByText("Congrats! You won!")).toBeDefined();
   });
 });
